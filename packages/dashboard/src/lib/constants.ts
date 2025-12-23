4bdcb778636ec053c8074202908c2fd238acafc1
kollucharan <kollucharanreddy@gmail.com>
2025-12-23 19:09:31 +0530

    chnaged url

--- packages/dashboard/src/lib/constants.ts
+++ packages/dashboard/src/lib/constants.ts
@@ -1,3 +1,4 @@
-export const API_URI = process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:4000';
+// export const API_URI = process.env.NEXT_PUBLIC_API_URI ?? 'http://localhost:4000';
+export const API_URI = '/api';
 
 export const NO_AUTH_ROUTES = ['/auth/signup', '/auth/login', '/auth/reset', '/unsubscribe/[id]', '/subscribe/[id]'];
